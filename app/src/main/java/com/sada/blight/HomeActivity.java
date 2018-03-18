023406b98e7f19d6721abf967b8055504bc8ce08
Shantanu Pramanik <srs.shaan@gmail.com>
2018-03-18 19:43:42 +0530

    Login Screen

--- app/src/main/java/com/sada/blight/HomeActivity.java
+++ app/src/main/java/com/sada/blight/HomeActivity.java
@@ -1,13 +1,30 @@
 package com.sada.blight;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class HomeActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mAuth = FirebaseAuth.getInstance();
+        if (mAuth.getCurrentUser() == null) {
+            startActivity(new Intent(getApplicationContext(), LoginActivity.class));
+        }
     }
 }
